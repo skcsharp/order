d5747977a501b23a03d5bf92037d9cf16a26e055
SKCC <SKCC@SKCC16D00057.SKCC.NET>
2020-07-03 13:34:32 +0900

    first commit

---
+++ src/main/java/demo/PolicyHandler.java
@@ -0,0 +1,16 @@
+package demo;
+
+import demo.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onEvent(@Payload String message) { }
+
+}
